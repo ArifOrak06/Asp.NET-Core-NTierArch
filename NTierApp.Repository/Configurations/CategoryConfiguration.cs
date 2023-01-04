44bf217ca268fe3a7b56b385c1ef5f2a969dbed2
Arif ORAK <ariforak91@gmail.com>
2023-01-05 01:22:16 +0300

    Context ve İlişkiler Eklendi.

---
+++ NTierApp.Repository/Configurations/CategoryConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NTierApp.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NTierApp.Repository.Configurations
+{
+    internal class CategoryConfiguration : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Id).UseIdentityColumn(); //def =1
+            builder.Property(x => x.Name).IsRequired().HasMaxLength(50);
+            builder.ToTable("Categories");
+
+
+        }
+    }
+}
--- NTierApp.Repository/Contexts/AppDbContext.cs
+++ NTierApp.Repository/Contexts/AppDbContext.cs
@@ -3,6 +3,7 @@ using NTierApp.Core.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -15,10 +16,18 @@ namespace NTierApp.Repository.Contexts
         {
 
         }
-
+        
         public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
         public DbSet<ProductFeature> ProductFeatures { get; set; }
+        
+        protected  override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //Configuration classları Assembly olarak tanımlandı.
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+            base.OnModelCreating(modelBuilder);
+        }
 
     }
 }
